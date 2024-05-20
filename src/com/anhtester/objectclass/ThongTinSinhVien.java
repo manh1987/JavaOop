3a75a32e7ab0d7246c578210740dd7160c9fb322
manh1987 <dinhvanmanh1987@gmail.com>
2024-05-20 22:46:32 +0700

    manh

---
+++ src/com/anhtester/objectclass/ThongTinSinhVien.java
@@ -0,0 +1,12 @@
+package com.anhtester.objectclass;
+
+public class ThongTinSinhVien {
+    public static void main(String[] args) {
+
+        //Khai báo đối tượng class để truy xuất thông tin trong một class
+        SinhVien sv1 = new SinhVien();
+
+        //Gọi thành phần trong class thông qua đối tượng
+        System.out.println(sv1.);
+    }
+}
